eced7b2780898e93694fdd2d491813136f4db83e
feet <weldersilvasurf@gmail.com>
2022-06-23 12:27:22 -0300

    objeto e comandos

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = ["Welder","Luan","Felipe","Cosme","Gabriel"];
+console.log (nome);
+
+nome.sort();
+console.log(nome);
+
+nome.pop(4);
+console.log (nome);
+
+nome.splice (4,2,"Mariana","Luccas");
+console.log (nome);
+
+nome.splice (0,1);
+console.log(nome);
+
+let numbers = [7,5,6,3,8,9,2,1,4];
+numbers.sort();
+console.log(numbers);
